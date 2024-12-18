978f14b2dd40b07062ae62423ace7b2a2fa30065
Jacob Rambarran <jacobrambarran@gmail.com>
2024-12-18 03:57:57 -0500

    added clipboard pasting

--- cli_inq.py
+++ cli_inq.py
@@ -1,9 +1,6 @@
 from InquirerPy import inquirer
+import clipboard
 from functions import handle_download_options, search_manga, extract_id_from_url
-
-
-
-
 
 
 def main():
@@ -30,12 +27,8 @@ def main():
         # Main menu with URL, Search, and Exit
         main_action = inquirer.select(
             message="Main menu:",
-            choices=[
-                "Enter URL",
-                "Search for manga",
-                "Exit"
-            ],
-            vi_mode=True, 
+            choices=["Enter URL", "Search for manga", "Exit"],
+            vi_mode=True,
         ).execute()
 
         if main_action == "Exit":
@@ -43,7 +36,9 @@ def main():
             break
 
         elif main_action == "Enter URL":
-            url = inquirer.text(message="Enter the manga URL:").execute()
+            cb = clipboard.paste()
+            url = inquirer.text(message="Enter the manga URL:",
+                                default=cb).execute()
             manga_id = extract_id_from_url(url)
             if manga_id:
                 handle_download_options(manga_id)
@@ -51,20 +46,23 @@ def main():
                 print("Invalid URL or ID not found.")
 
         elif main_action == "Search for manga":
-            title = inquirer.text(message="Enter the manga title to search:").execute()
+            title = inquirer.text(
+                message="Enter the manga title to search:").execute()
             manga_choices = search_manga(title)
 
             if manga_choices:
                 manga_choices.append({'name': 'Go Back', 'value': 'go_back'})
                 selected_manga_id = inquirer.select(
                     message="Select the manga:",
-                    choices=manga_choices, 
-                    vi_mode=True, 
+                    choices=manga_choices,
+                    vi_mode=True,
                 ).execute()
 
                 if selected_manga_id != 'go_back':
                     handle_download_options(selected_manga_id)
             else:
                 print("No results found for that title.")
+
+
 if __name__ == "__main__":
     main()
