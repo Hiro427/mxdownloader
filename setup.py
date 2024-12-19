22cab00424dd3ec59dbd17d97a704bab47fab548
Jacob Rambarran <jacobrambarran@gmail.com>
2024-12-19 17:41:20 -0500

    added new details for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as f:
-    description = f.read()
-
 
 def parse_requirements(file_path):
     with open(file_path, 'r') as file:
@@ -14,14 +11,24 @@ def parse_requirements(file_path):
 
 setup(
     name="mxdownloader",
-    version="0.2",
+    version="1.0.1",
     packages=find_packages(),
-    install_requires=parse_requirements('requirements.txt'),
     entry_points={
         "console_scripts": [
             "mdx = mxdownloader:main",
         ],
     },
-    long_description=description,
-    long_description_content_type='text/markdown',
+    description="Download Manga using the MangaDex API",
+    license="MIT",
+    long_description=
+    "Visit the project's [GitHub page](https://github.com/Hiro427/mxdownloader) for more info. ![Screenshot from 2024-09-23 16-35-13](https://github.com/user-attachments/assets/043f42e7-6fb9-4634-bdc2-bf1030577f44)",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hiro427/mxdownloader",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    author="Jacob Rambarran",
+    install_requires=parse_requirements('requirements.txt'),
 )
